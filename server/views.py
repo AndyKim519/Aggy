bfac4d20772d8e55749f16be94cbb56568094467
Andy Kim <andy519kim@berkeley.edu>
2023-10-28 10:20:24 -0700

    minor typo views

--- server/views.py
+++ server/views.py
@@ -5,13 +5,16 @@ from logic import *
 
 app_views = Blueprint('app_views', __name__)
 
+
 @app_views.route('/createnetwork', methods=['POST'])
 def createNetwork():
     req_data = request.get_json()
     networkName = req_data.get('networkName')
     networkDetails = req_data.get('networkDetails')
     presetQuestions = req_data.get('presetQuestions')
-    newNetwork(networkName=networkName, networkDetails=networkDetails, presetQuestions=presetQuestions)
+    newNetwork(networkName=networkName, networkDetails=networkDetails,
+               presetQuestions=presetQuestions)
+
 
 @app_views.route('/postaudio', methods=['POST'])
 def postAudio():
@@ -22,6 +25,7 @@ def postAudio():
     userInput(networkID, convertedText)
     return
 
+
 @app_views.route('/postforsummary', methods=['POST'])
 def postForSummary():
     req_data = request.get_json()
@@ -29,13 +33,11 @@ def postForSummary():
     summary = handleSummary(networkID)
     return summary
 
-@app.views.route('/postforquestion', methods=['POST'])
+
+@app_views.route('/postforquestion', methods=['POST'])
 def postForQuestion():
     req_data = request.get_json()
     networkID = req_data.get('networkID')
     question = req_data.get('question')
     response = handleQuestion(networkID, question)
     return response
-
-
-
